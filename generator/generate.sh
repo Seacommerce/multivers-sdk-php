64934188b467a0055a5e1b0ed71adb5cf42e0b42
Sil Moelker <smoelker@gmail.com>
2019-03-11 14:31:13 +0100

    spec folder

--- generator/generate.sh
+++ generator/generate.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-openapi-generator generate -g php -o ../ -i ../../spec/v1.10.0/unit4-multivers-webapi-v1.10.0.yaml -c config.json
+openapi-generator generate -g php -o ../ -i ../../multivers-spec/v1.10.0/unit4-multivers-webapi-v1.10.0.yaml -c config.json
